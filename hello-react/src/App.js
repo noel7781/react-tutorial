ce381955b85458b7bb5dca4e335df458cb78515f
조성진 <noel7781@naver.com>
2021-07-20 21:56:21 +0900

    fix

---
+++ hello-react/src/App.js
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Fragment } from "react";
+
+function App() {
+  const name = "react";
+  return (
+    <>
+      <div className="react">{name}</div>
+      <h1>asdsd</h1>
+      <h2>ads</h2>
+      <h3>asdas</h3>
+      <h4>fsadg</h4>
+      <input />
+    </>
+  );
+}
+
+export default App;
