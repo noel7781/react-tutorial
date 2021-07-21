d680165312226293681d1af1b0af8db94aaec9e9
조성진 <noel7781@naver.com>
2021-07-21 19:35:53 +0900

    Component

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,19 +1,10 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { Fragment } from "react";
+import React from 'react';
+import MyComponent from './MyComponent';
+import Counter from './Counter';
+import Say from './Say';
 
-function App() {
-  const name = "react";
-  return (
-    <>
-      <div className="react">{name}</div>
-      <h1>asdsd</h1>
-      <h2>ads</h2>
-      <h3>asdas</h3>
-      <h4>fsadg</h4>
-      <input />
-    </>
-  );
-}
+const App = () => {
+  return <Say />;
+};
 
 export default App;
---
+++ hello-react/src/Counter.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         number: 0,
+    //         fixedNumber: 0,
+    //     };
+    // }
+    state = {
+        number: 0,
+        fixedNumber: 0,
+    }
+    render() {
+        const { number, fixedNumber } = this.state;
+        return (
+            <div>
+                <h1>{number}</h1>
+                <h2>바뀌지 않는 값: {fixedNumber}</h2>
+                <button onClick={() => {
+                    this.setState( {
+                        number: number+1
+                    }, () => {
+                        console.log("setState가 호출됨.");
+                        console.log(this.state);
+                    })
+                }}
+                >
+                +1
+                </button>
+            </div>
+        );
+    }
+}
+
+export default Counter;
