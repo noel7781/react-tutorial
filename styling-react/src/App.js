bd7d00d078b1cb7ab2259d81097fb97bd9160af2
조성진 <noel7781@naver.com>
2021-08-29 00:19:29 +0900

    react-todo

---
+++ styling-react/src/App.js
@@ -0,0 +1,16 @@
+import SassComponent from './SassComponent';
+import { Component } from 'react';
+import CSSModule from './CSSModule';
+import StyledComponent from './StyledComponent';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <StyledComponent />
+      </div>
+    );
+  }
+}
+
+export default App;
